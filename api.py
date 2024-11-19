fc042cb79d27e31e413e52f2ea8bff61bfc2f142
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-11-19 23:03:45 +0545

    create enum of diff- zone

--- api.py
+++ api.py
@@ -2,3 +2,16 @@ import enum
 from typing import Annotated
 from livekit.agents import llm
 import logging
+
+logger = logging.getLogger("temperature-control")
+logger.setLevel(logging.INFO)
+
+class Zone(enum.Enum):
+    LIVING_ROOM = "living_room"
+    BEDROOM = "bedroom"
+    KITCHEN = "kitchen"
+    BATHROOM = "bathroom"
+    OFFICE = "office"
+
+class AssistantFnc(llm.FunctionContext):
+    
